859e0f7209f5d3208c16766cadcaaad3347e3fc4
Tony Atkins <tony@raisingthefloor.org>
2018-08-16 14:44:12 +0200

    GPII-3278: Updated to newest testem version and removed workaround.

--- tests/testem-fixtures/coverage-fixtures/testem-package-relative-paths.js
+++ tests/testem-fixtures/coverage-fixtures/testem-package-relative-paths.js
@@ -19,13 +19,6 @@ var testemComponent = gpii.tests.testem.harness({
     },
     contentDirs: {
         nm: "%gpii-testem/node_modules"
-    },
-    // NOTE: The next option is only required because of a Testem bug WRT the cwd option on Windows, and can be removed
-    // once this issue is resolved:
-    //
-    // https://github.com/testem/testem/issues/1286
-    testemOptions: {
-        unsafe_file_serving: true
     }
 });
 
